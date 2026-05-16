4873f6149847ed7b8a714d5c9b0166247e4ad252
Carlos <loyolacarlos162@gmail.com>
2026-05-16 21:38:09 +0800

    add: new updates auto-download-rider-app

--- client/src/pages/download_app_page.jsx
+++ client/src/pages/download_app_page.jsx
@@ -1,14 +1,24 @@
+import { useEffect } from "react";
+
 const Download_Rider_App = () => {
 
     const APK_LINK = "https://github.com/devLos12/mern-efarmerShub/releases/download/v1.2.0/application-cf64accb-28bb-4ec2-8eb4-0fc8a8a22550.apk";
 
-    return (
-        <div>
-            <a href={APK_LINK}>
-                Download Rider App
-            </a>
-        </div>
-    )
+    useEffect(() => {
+        const a = document.createElement("a");
+        a.href = APK_LINK;
+        a.download = "EFarmersHub-Rider.apk";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+
+        // balik sa email after trigger ng download
+        setTimeout(() => {
+            window.history.back();
+        }, 2000);
+    }, []);
+
+    return null
 
 }
 
