21f3f6686327361c06fc218dc0f05e7617fa9a37
Carlos <loyolacarlos162@gmail.com>
2026-05-16 21:30:59 +0800

    new updates

--- client/src/pages/download_app_page.jsx
+++ client/src/pages/download_app_page.jsx
@@ -1,14 +1,15 @@
-
-
-
 const Download_Rider_App = () => {
+
+    const APK_LINK = "https://github.com/devLos12/mern-efarmerShub/releases/download/v1.2.0/application-cf64accb-28bb-4ec2-8eb4-0fc8a8a22550.apk";
 
     return (
         <div>
-            download app here 
+            <a href={APK_LINK}>
+                Download Rider App
+            </a>
         </div>
     )
 
-}   
+}
 
 export default Download_Rider_App;
